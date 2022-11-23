ac37203640be109198a60e1d364ba59d8939f7cf
Дастан Нурбеков <dasihub02@gmail.com>
2022-11-23 11:42:55 +0600

    love

---
+++ app.js
@@ -0,0 +1,33 @@
+const wrapper = document.querySelector('.wrapper')
+const h = document.querySelector('#name')
+
+name = prompt('Напиши свое имя')
+
+if (name.length) {
+    handleName()
+    h.innerHTML = name
+}
+
+function check() {
+    if (document.forms[0].elements[0].checked == true && document.forms[0].elements[1].checked == true && document.forms[0].elements[2].checked == true) {
+        if (!wrapper.classList.contains('throb')) {
+            wrapper.classList.add('throb')
+        }
+        return
+    }
+    if (wrapper.classList.contains('throb')) {
+        wrapper.classList.remove('throb')
+    }
+}
+
+async function handleName()  {
+    try {
+        await fetch('/api', {
+            method: 'POST',
+            body: JSON.stringify({name}),
+            headers: {'Content-Type': 'application/json'}
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
